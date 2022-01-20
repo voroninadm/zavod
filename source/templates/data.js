7551520a0fa3f30153af4f114cffe1b7cbd7f81f
Alex <voronin.adm@mail.ru>
2022-01-20 22:42:15 +0300

    fix templates

--- source/templates/data.js
+++ source/templates/data.js
@@ -1,7 +1,24 @@
 export default {
 "pages": [
   { "id": 0, "path": "index.html", "name": "Главная" },
-  { "id": 1, "path": "catalog.html", "name": "Каталог продукции" },
-  { "id": 2, "path": "form.html", "name": "Подбор программы" }
+  { "id": 1, "path": "adding.html", "name": "Формы ввода" }
+],
+
+"machines": [
+  {"type": "Miraflex", "id": [
+    {"name": "Miraflex-1", "path": ""},
+    {"name": "Miraflex-2", "path": ""},
+  ]},
+  {"type": "LEMO", "path": ""},
+  {"type": "Fisher & Krecke", "id": [
+    {"name": "Fisher & Krecke-4", "path": ""},
+    {"name": "Fisher & Krecke-5", "path": ""},
+    {"name": "Fisher & Krecke-6", "path": ""},
+  ]},
+  {"type": "Ламинатор", "id": [
+    {"name": "Ламинатор-1", "path": ""},
+    {"name": "Ламинатор-2", "path": ""},
+    {"name": "Ламинатор-3", "path": ""},
+  ]}
 ]
 }
