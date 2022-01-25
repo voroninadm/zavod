993130ff70d73fbcf1e1a1a470a9ab2d0b233e57
Alex <voronin.adm@mail.ru>
2022-01-25 23:05:12 +0300

    add machines pages

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ import data from "./source/templates/data.js";
 const { src, dest, watch, series, parallel } = gulp;
 
 export function processMarkup () {
-  return src("./source/html/*.html")
+  return src(["./source/html/*.html", "./source/html/machines/*.html"])
     .pipe(twig({
       data: data
     }))
@@ -36,7 +36,7 @@ export function processMarkup () {
 }
 
 export function validateMarkup () {
-  return src("./source/html/*.html")
+  return src(["./source/html/*.html", "./source/html/machines/*.html"])
   .pipe(twig({
     data: data
   }))
--- source/templates/data.js
+++ source/templates/data.js
@@ -1,24 +1,18 @@
 export default {
-"pages": [
-  { "id": 0, "path": "index.html", "name": "Главная" },
-  { "id": 1, "path": "adding.html", "name": "Формы ввода" }
-],
+  "pages": [
+    { "id": 0, "path": "index.html", "name": "Главная" },
+    { "id": 1, "path": "addings.html", "name": "Формы ввода" }
+  ],
 
-"machines": [
-  {"type": "Miraflex", "id": [
-    {"name": "Miraflex-1", "path": ""},
-    {"name": "Miraflex-2", "path": ""},
-  ]},
-  {"type": "LEMO", "path": ""},
-  {"type": "Fisher & Krecke", "id": [
-    {"name": "Fisher & Krecke-4", "path": ""},
-    {"name": "Fisher & Krecke-5", "path": ""},
-    {"name": "Fisher & Krecke-6", "path": ""},
-  ]},
-  {"type": "Ламинатор", "id": [
-    {"name": "Ламинатор-1", "path": ""},
-    {"name": "Ламинатор-2", "path": ""},
-    {"name": "Ламинатор-3", "path": ""},
-  ]}
-]
+  "machines": [
+    { "id": 0, "path": "miraflex1.html", "name": "Miraflex-1" },
+    { "id": 1, "path": "miraflex2.html", "name": "Miraflex-2" },
+    { "id": 2, "path": "lemo.html", "name": " LEMO" },
+    { "id": 3, "path": "fisher4.html", "name": "Fisher & Krecke-4" },
+    { "id": 4, "path": "fisher5.html", "name": "Fisher & Krecke-5" },
+    { "id": 5, "path": "fisher6.html", "name": "Fisher & Krecke-6" },
+    { "id": 6, "path": "laminator1.html", "name": "Ламинатор-1" },
+    { "id": 7, "path": "laminator2.html", "name": "Ламинатор-2" },
+    { "id": 8, "path": "laminator3.html", "name": "Ламинатор-3" },
+  ]
 }
