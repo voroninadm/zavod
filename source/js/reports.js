86dd48d3f2525b55874c609924d7f8c69d97f5b3
Alexey Voronin <voronin.adm@mail.ru>
2022-03-30 10:10:29 +0300

    report form

--- source/js/reports.js
+++ source/js/reports.js
@@ -1,7 +1,9 @@
 const tableBody = document.querySelector('.table-sort__body');
 const filters = document.querySelector('.filters');
+const rowData = tableBody.querySelectorAll('.row-data')
 const hidingClass = 'visually-hidden';
 
+rowData.forEach((row) => row.classList.add(hidingClass));
 
 filters.addEventListener('change', (evt) => {
   const target = evt.target.name;
